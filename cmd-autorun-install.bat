5994fa2d3fc3ba2b391f17c2474eb28d5eea677d
Chris Watson <chris.watson@7digital.com>
2013-02-26 12:48:52 +0000

    Add CMD autorun installer, to use with aliases

---
+++ cmd-autorun-install.bat
@@ -0,0 +1,19 @@
+@echo off
+set dir=%~dp0
+set autorunfilename=cmd-autorun.bat
+set autorunfile="%dir%%autorunfilename%"
+set aliasfilename=cmd-aliases
+set aliasfile="%dir%%aliasfilename%"
+
+(
+	echo cls
+	echo @echo off
+	echo doskey /macrofile=./%aliasfilename%
+	echo dir
+) > %autorunfile%
+
+(
+	echo test=echo test!
+) > %aliasfile%
+
+reg add "hklm\software\microsoft\command processor" /v Autorun /t reg_sz /d \"%autorunfile%\"
