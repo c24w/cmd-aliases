fff4a7fa0b3fec6558a4aa54f2b14a2b0147f16e
Chris Watson <chris.watson@7digital.com>
2013-03-01 17:01:40 +0000

    Improve console output

--- cmd-autorun-install.bat
+++ cmd-autorun-install.bat
@@ -4,18 +4,21 @@ set autorunfilename=cmd-autorun.bat
 set autorunfile="%dir%%autorunfilename%"
 set aliasfilename=cmd-aliases
 set aliasfile="%dir%%aliasfilename%"
+set regLoc=HKLM\Software\Microsoft\Command Processor
 
+echo Creating %autorunfile%
 (
-	echo cls
 	echo @echo off
 	echo doskey /macrofile=%aliasfile%
-	echo dir
 ) > %autorunfile%
 
+echo Creating %aliasfile%
 (
 	echo test=echo test!
 ) > %aliasfile%
 
-reg add "hklm\software\microsoft\command processor" /v Autorun /t reg_sz /d \"%autorunfile%\"
+echo Adding to registry
+reg add "%regLoc%" /v Autorun /t reg_sz /d \"%autorunfile%\"
 
+echo Deleting "%~f0"
 DEL "%~f0"
